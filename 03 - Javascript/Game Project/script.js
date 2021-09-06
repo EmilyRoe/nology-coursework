47b0b9ecd6efd01d36a7e10a569371cfd8601984
EmilyRoe <61165374+EmilyRoe@users.noreply.github.com>
2021-09-06 07:24:20 +0100

    pseudo coding avatar jump

---
+++ 03 - Javascript/Game Project/script.js
@@ -0,0 +1,11 @@
+// EVENT LISTENER TO REMOVE AVATAR FROM DIV AND RETURN ON A TIMER
+const avatar = document.querySelector (".avatar")
+avatar.addEventListener ("keyup", () => {
+    if (keypress == "upArrow") {
+        //remove avatar class i.e. $(this).removeClass("saved"); ?
+        //setTimeout for class to be removed for x milliseconds
+    }
+})
+
+// Here you need to add an eventListener so that the avatar appears in the above div for x milliseconds
+// OR does do you just add it in above...?
