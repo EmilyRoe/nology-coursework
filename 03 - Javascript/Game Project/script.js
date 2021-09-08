54afbde9f59bfd7f1a8d94c07b0ed939b93e987a
EmilyRoe <61165374+EmilyRoe@users.noreply.github.com>
2021-09-08 08:02:26 +0100

    Pseudo coded adding in obstacles setInterval

--- 03 - Javascript/Game Project/script.js
+++ 03 - Javascript/Game Project/script.js
@@ -22,6 +22,30 @@ jumpingAvatar.addEventListener ("keyUp", () => {
     }
 })
 
-
 // NOTE: you need to figure out how to get the up arrow button to apply to the div and not the webpage as a whole 
 // as it is currently functioning as a scroll
+
+
+// OBSTACLES
+let state = 1;
+
+function moveObstacle(){
+	
+let thisObstacle = document.querySelectorAll (".obstacle");
+    if (state == 1){
+        thisObstacle += 100;
+    } else if (state == 2){ // i.e. if img is on the left
+        // return to state 1
+    }
+
+function obstacleMove(){
+    obstacle=setInterval(obstacle, 1000);
+    }
+
+
+// You want the img to move from right to left across the div and for the next div to only appear once the first one reaches the 
+//end of the div
+
+// NEXT STEPS:
+// Hiding the images in the divs so that it appears the avatar is running towards them when the game starts
+// Telling the images to appear at intervals for when the game starts
